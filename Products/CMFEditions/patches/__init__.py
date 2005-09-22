f14c520e0f137713f038824ce79861ce224d2778
Gregoire Weber <gregweb@incept.ch>
2005-09-22 14:09:17 +0000

    - removed CatalogMultiplex monkey patch (bug corrected in Archetypes 1.3.4)
    svn path=/CMFEditions/trunk/; revision=12101

--- Products/CMFEditions/patches/__init__.py
+++ Products/CMFEditions/patches/__init__.py
@@ -1,47 +1,26 @@
-try:
-    from Products.Archetypes.CatalogMultiplex import CatalogMultiplex
-except ImportError:
-    pass
-else:
-    from Acquisition import aq_base
-    
-    def __recurseIntoOpaqueItems(self, name, *args):
-        """Recurse in opaque items (new in CMF 1.5).
-        """
-        # This is from CMFCore/CMFCatalogAware.py
-        for ob in self.opaqueValues():
-            s = getattr(ob, '_p_changed', 0)
-            if hasattr(aq_base(ob), name):
-                getattr(ob, name)(*args)
-            if s is None: ob._p_deactivate()
+#########################################################################
+# Copyright (c) 2005 Gregoire Weber. 
+# All Rights Reserved.
+# 
+# This file is part of CMFEditions.
+# 
+# CMFEditions is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# CMFEditions is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with CMFEditions; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#########################################################################
+"""Collection of monkey patches.
 
-    CatalogMultiplex_manage_afterAdd = CatalogMultiplex.manage_afterAdd
-    def manage_afterAdd(self, item, container):
-        """Recurse into opaque items before doing the usual stuff.
-        """
-        __recurseIntoOpaqueItems(self, 'manage_afterAdd', item, container)
-        CatalogMultiplex_manage_afterAdd(self, item, container)
-    CatalogMultiplex.manage_afterAdd = manage_afterAdd
+$Id: $
+"""
 
-    CatalogMultiplex_manage_afterClone = CatalogMultiplex.manage_afterClone
-    def manage_afterClone(self, item):
-        """Recurse into opaque items before doing the usual stuff.
-        """
-        __recurseIntoOpaqueItems(self, 'manage_afterClone', item)
-        CatalogMultiplex_manage_afterClone(self, item)
-    CatalogMultiplex.manage_afterClone = manage_afterClone
-    
-    CatalogMultiplex_manage_beforeDelete = CatalogMultiplex.manage_beforeDelete
-    def manage_beforeDelete(self, item, container):
-        """Recurse into opaque items before doing the usual stuff.
-        """
-        __recurseIntoOpaqueItems(self, 'manage_beforeDelete', item, container)
-        CatalogMultiplex_manage_beforeDelete(self, item, container)
-    CatalogMultiplex.manage_beforeDelete = manage_beforeDelete
-
-    # just remove all newly added names to not pollute the namespace 
-    # of the caller
-    del CatalogMultiplex
-    del manage_afterAdd
-    del manage_afterClone
-    del manage_beforeDelete
+# None at the moment
