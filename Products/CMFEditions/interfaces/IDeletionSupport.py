1ae46bdede68364ccddf4e21dbd6271eedc91092
Gregoire Weber <gregweb@incept.ch>
2006-05-09 21:01:38 +0000

    - improved documentation - added module introduction about the deletion strategy
    svn path=/CMFEditions/branches/gregweb-delete_support/; revision=23203

--- Products/CMFEditions/interfaces/IDeletionSupport.py
+++ Products/CMFEditions/interfaces/IDeletionSupport.py
@@ -28,6 +28,21 @@ Version deletion support consists of ...
 2. and a deletion policy which at save time may automatically delete
    versions from a resources history.
 
+The main strategy with version deletion support is that the layers above
+the storage layer may be kept untouched. Examples:
+
+- For manual deletion of a resources version the storage tool has to be 
+  asked. No upper layers are invoked.
+- For automatic deletion of a resources version a policy (implemented as 
+  tool) may be installed performing the necessary deletion tasks on every
+  save operation. The default implementation will be able to only keep 
+  the n current versions.
+- If the archivist requests for a deleted version of a resource it just 
+  gets another one (e.g. the next older one, depending on the policy).
+- Deleted items are just skiped when iterating over the history without 
+  the for loops body code noticing anything (fortunately we already look 
+  up items lazily).
+
 $Id$
 """
 
@@ -56,20 +71,23 @@ class IDeletionSupport(Interface):
         Return a ``IVersionData`` object wich may contain a deleted 
         version of an object.
         
-        Reinterpretation of ``retrieve`` and ``getHistory``:
+        Reinterpretation of ``retrieve`` and ``getHistory`` is necessary:
         
-        In constrast the "normal" ``retrieve`` and ``__getattr__`` now 
-        has to return a substitute if an ``IDeletionPolicy`` was found. 
-        As well when a specific version is read from the history returned 
-        from ``getHistory``.
+        The "normal" ``retrieve`` and ``__getattr__`` now has to return 
+        a substitute if an ``IDeletionPolicy`` was found. As well when 
+        a specific version is read from the history returned from 
+        ``getHistory``.
         """
 
-    def getEffectiveHistoriesLength(history_id):
+    def getEffectiveLength(history_id):
         """Return the Length of the History Ignoring Deleted Versions
         
         In contrast the ``__len__`` method of the history returned still 
         has to return the length of the history *including* the deleted
         version (because upper layers may try to count from the end).
+        
+        XXX Check if it is necessary to be backwards compatible with 
+            ``__len__``.
         
         ``__iter__`` from ``IHistory`` shall always return the next non
         deleted version of the resources history.
@@ -83,8 +101,14 @@ class IDeletionPolicy(Interface):
     """
     
     def delete(history_id, metadata={}):
-        """Delete a Version from the History According a Policy
+        """Delete Versions from the History According a Policy
+        
+        The Policy has full control whole history of the resource and
+        may decide to delete none, one or even more than one version.
         
         The metadata passed may be used to store informations about the 
         reasons of the deletion.
+        
+        Return ``True`` if the current version shall not be saved and 
+        discarded.
         """
