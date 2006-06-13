3edf6846eb597e0a4e6ea3321aceba47a80a655e
Gregoire Weber <gregweb@incept.ch>
2006-06-13 21:47:36 +0000

    - corrected var name typo
    svn path=/CMFEditions/trunk/; revision=24839

--- Products/CMFEditions/ZVCStorageTool.py
+++ Products/CMFEditions/ZVCStorageTool.py
@@ -377,7 +377,7 @@ class ZVCStorageTool(UniqueObject, SimpleItem, ActionProviderBase):
         
         if deletedHistories:
             deletedAverage = "%.1f" % \
-                round(float(existingVersions)/deletdHistories, 1)
+                round(float(existingVersions)/deletedHistories, 1)
         else:
             deletedAverage = "n/a"
         
