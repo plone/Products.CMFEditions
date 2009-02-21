8e2c5a94e7fd524a8eb92d5caaa44488a785e32d
Hanno Schlichting <hanno@hannosch.eu>
2009-02-21 17:41:30 +0000

    Remove unsused code snippet
    svn path=/Products.CMFEditions/trunk/; revision=81156

--- Products/CMFEditions/tests/__init__.py
+++ Products/CMFEditions/tests/__init__.py
@@ -1,18 +1,3 @@
 """\
 Unit test package for CMFEditions
 """
-
-def installProduct(portal, name, optional=False):
-    """Use this to optionaly load products.
-    
-    Returns True if a product could be installed.
-    """
-    quickinstaller = portal.portal_quickinstaller
-    if optional:
-        try:
-            quickinstaller.installProduct(name)
-        except AttributeError:
-            return False
-    else:
-        quickinstaller.installProduct(name)
-    return True
