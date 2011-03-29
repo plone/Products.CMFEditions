plone/Products.CMFEditions

commit 0dd4a7c07bb84299c4a90c8967baf8065d324735

Backport from trunk : Do not provide "Compare to ... revision" link in versions history actions if content type has no diffable field.
svn path=/Products.CMFEditions/branches/1.2/; revision=228762

Products/CMFEditions/skins/CMFEditions/versions_history_form.pt
@@ -9,7 +9,9 @@
                    version_id python:request.get('version_id', None);
                    history python:pr.getHistoryMetadata(context);
                    revisions python:history and history.getLength(countPurged=False) or 0;
-                   histrange python:range(revisions-1, -1, -1)"
+                   histrange python:range(revisions-1, -1, -1);
+                   portal_diff nocall:here/portal_diff|nothing;
+                   can_diff python:portal_diff and len(portal_diff.getDiffForPortalType(here.portal_type)) > 0;"
                    tal:condition="python: pr.isVersionable(context)">
     <h1 class="documentFirstHeading"
         i18n:translate="versions_of_title">Revisions of
@@ -73,7 +75,7 @@
                   </td>
                   <td class="VersionActions">
                     <ul>
-                        <li tal:condition="exists:here/portal_diff">
+                        <li tal:condition="can_diff">
                             <a href=""
                                style="text-decoration: none"
                                tal:attributes="href string:$here_url/version_diff?version_id1=current&amp;version_id2=${reverted_vid}">
@@ -123,7 +125,7 @@
                   </td>
                   <td class="VersionActions">
                     <ul>
-                        <tal:diffs condition="exists:here/portal_diff">
+                        <tal:diffs condition="can_diff">
                         <li tal:condition="not:current_version">
                             <a href=""
                                style="text-decoration: none"

commit 30434018edadf138b48b325601bf1baa1fd75c7d

getDiffForPortalType is called in unrestricted code. fix unauthorized error introduced by r228762
svn path=/Products.CMFEditions/branches/1.2/; revision=236672

Products/CMFEditions/browser/configure.zcml
@@ -16,5 +16,13 @@
         permission="cmf.ModifyPortalContent"
         />
 
+    <browser:page
+        for="*"
+        name="historyview"
+        class=".diff.HistoryView"
+        allowed_attributes="can_diff"
+        permission="zope2.View"
+        />
+
 </configure>
 

Products/CMFEditions/browser/diff.py
@@ -57,3 +57,12 @@ class DiffView(BrowserView):
         return self.template()
 
 
+class HistoryView(BrowserView):
+
+    def can_diff(self, portal_type):
+        """Return True if content is diffable
+        """
+        context = self.context
+        portal_diff = getToolByName(context, 'portal_diff', None);
+        return portal_diff and len(portal_diff.getDiffForPortalType(context.portal_type)) > 0
+

Products/CMFEditions/skins/CMFEditions/versions_history_form.pt
@@ -10,8 +10,8 @@
                    history python:pr.getHistoryMetadata(context);
                    revisions python:history and history.getLength(countPurged=False) or 0;
                    histrange python:range(revisions-1, -1, -1);
-                   portal_diff nocall:here/portal_diff|nothing;
-                   can_diff python:portal_diff and len(portal_diff.getDiffForPortalType(here.portal_type)) > 0;"
+                   view nocall:context/@@historyview;
+                   can_diff view/can_diff"
                    tal:condition="python: pr.isVersionable(context)">
     <h1 class="documentFirstHeading"
         i18n:translate="versions_of_title">Revisions of