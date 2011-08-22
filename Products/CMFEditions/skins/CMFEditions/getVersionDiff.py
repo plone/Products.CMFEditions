388be73c23a43e81cadcc39302f5cd7285ada87a
rafaelbco <rafaelbco@localhost>
2011-08-22 20:18:25 +0000

    Allow to translate the string "current" in the version_diff template.
    svn path=/Products.CMFEditions/trunk/; revision=243681

--- Products/CMFEditions/skins/CMFEditions/getVersionDiff.py
+++ Products/CMFEditions/skins/CMFEditions/getVersionDiff.py
@@ -9,6 +9,7 @@
 ##
 
 from Products.CMFCore.utils import getToolByName
+from Products.CMFEditions import CMFEditionsMessageFactory as _
 
 retrieve = context.portal_repository.retrieve
 
@@ -20,7 +21,9 @@ else:
 ob2 = retrieve(context, int(id2)).object
 base_id = ob1.getId()
 portal_diff = getToolByName(context, 'portal_diff')
+
+# Translate the revision IDs to enable translation of the string "current".
 diff = portal_diff.createChangeSet(ob2, ob1,
-                                   id1=ob2.getId()+' (rev. %s)'%id2,
-                                   id2=ob1.getId()+' (rev. %s)'%id1)
+                                   id1=ob2.getId()+' (rev. %s)'% context.translate(_(id2)),
+                                   id2=ob1.getId()+' (rev. %s)'% context.translate(_(id1)))
 return diff
