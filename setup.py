9aa9561abde329e11ff98b7d5fb592f049f77391
Maurits van Rees <maurits@vanrees.org>
2016-12-05 14:02:21 +0100

    Removed Plone 4.3 and 5.0 classifiers.
    It should still work fine on those versions,
    but in the new major 3.0 release we will start deleting revisions when removing an object.

--- setup.py
+++ setup.py
@@ -15,8 +15,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Framework :: Plone',
-        'Framework :: Plone :: 4.3',
-        'Framework :: Plone :: 5.0',
         'Framework :: Plone :: 5.1',
         'Framework :: Zope2',
     ],
