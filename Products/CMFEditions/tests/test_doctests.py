e8100eab51474156249d7bdd58aa640979afc2a9
Jens W. Klein <jk@kleinundpartner.at>
2018-09-14 14:22:04 +0200

    make doctest files rst

--- Products/CMFEditions/tests/test_doctests.py
+++ Products/CMFEditions/tests/test_doctests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from plone.testing import layered
-from Products.CMFEditions.testing import PRODUCTS_CMFEDITIONS_FUNCTIONAL_TESTING
+from Products.CMFEditions.testing import PRODUCTS_CMFEDITIONS_FUNCTIONAL_TESTING  # noqa
 
 import doctest
 import six
@@ -24,15 +24,26 @@ class DummyContent(object):
         self.id = obid
         self.__annotations__ = {}
 
-OPTIONFLAGS = (doctest.ELLIPSIS |
-               doctest.NORMALIZE_WHITESPACE |
-               doctest.REPORT_ONLY_FIRST_FAILURE)
+
+OPTIONFLAGS = (
+    doctest.ELLIPSIS |
+    doctest.NORMALIZE_WHITESPACE |
+    doctest.REPORT_ONLY_FIRST_FAILURE
+)
+
+
 def test_suite():
     suite = unittest.TestSuite()
     if six.PY2:
-        suite.addTest(layered(doctest.DocFileSuite('webdav_history.txt',
-                                    optionflags=OPTIONFLAGS,
-                                    package='Products.CMFEditions.tests',),
-                              layer=PRODUCTS_CMFEDITIONS_FUNCTIONAL_TESTING))
-    suite.addTest(doctest.DocFileSuite('large_file_modifiers.txt'))
+        suite.addTest(
+            layered(
+                doctest.DocFileSuite(
+                    'webdav_history.rst',
+                    optionflags=OPTIONFLAGS,
+                    package='Products.CMFEditions.tests',
+                ),
+                layer=PRODUCTS_CMFEDITIONS_FUNCTIONAL_TESTING,
+            )
+        )
+    suite.addTest(doctest.DocFileSuite('large_file_modifiers.rst'))
     return suite
