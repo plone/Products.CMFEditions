37f0ef2988a87076a722a5b7f5345242710f11eb
Maurits van Rees <maurits@vanrees.org>
2021-01-15 19:26:45 +0100

    Added upgrade step to remove broken AT modifiers from portal_modifier.
    I let the metadata.xml version jump from 4 to 10 to leave room on branch 3.x for version increases.
    
    Note that CMFEditions upgrade steps are applied automatically when running plone-upgrade,
    because it is in the `ADDON_LIST` in `CMFPlone.MigrationTool`.

--- Products/CMFEditions/setuphandlers.py
+++ Products/CMFEditions/setuphandlers.py
@@ -6,6 +6,11 @@ CMFEditions setup handlers.
 from Products.CMFCore.utils import getToolByName
 from Products.CMFEditions import StandardModifiers
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 def importVarious(context):
     """
@@ -31,3 +36,23 @@ def installSkipRegistryBasesPointersModifier(context):
     """Upgrade step to install the component registry bases modifier."""
     portal_modifier = getToolByName(context, 'portal_modifier', None)
     StandardModifiers.install(portal_modifier, ['SkipRegistryBasesPointers'])
+
+
+def removeBrokenModifiers(context):
+    """Remove broken modifiers.
+
+    For Plone 6 we have removed Archetypes support.
+    This includes removing classes for four Archetypes modifiers.
+    During normal usage you should not notice this.
+    But it is still better to remove them.
+    """
+    from Products.CMFEditions.interfaces.IModifier import IConditionalModifier
+
+    tool = getToolByName(context, 'portal_modifier', None)
+    for modifier_id, modifier in tool.objectItems():
+        if not IConditionalModifier.providedBy(modifier):
+            continue
+        if not modifier.isBroken():
+            continue
+        tool._delObject(modifier_id)
+        logger.info("Removed broken %s from portal_modifier.", modifier_id)
